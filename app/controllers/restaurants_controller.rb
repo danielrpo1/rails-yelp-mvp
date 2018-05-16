d505b6c45845f5c54b6b91c1be46a93025a13867
daniel restrepo <rpodaniel1@gmail.com>
2018-05-16 17:13:32 +0200

    rails new

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,45 @@
+class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+   def new
+    @restaurant = Restaurant.new
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    respond_to do |format|
+      if @restaurant.save
+        format.html { redirect_to @restaurant, notice: 'Restaurant was successfully created.' }
+        format.json { render :show, status: :created, location: @restaurant }
+      else
+        format.html { render :new }
+        format.json { render json: @restaurant.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+      @restaurant = Restaurant.find(params[:id])
+      @restaurant.destroy
+  end
+
+  private
+
+
+   def restaurant_params
+      params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+    end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Restaurant.destroy_all
+
+20.times do
+ Restaurant.create({
+   name: Faker::Company.name,
+   address: Faker::Address.city,
+   category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+ })
+end
